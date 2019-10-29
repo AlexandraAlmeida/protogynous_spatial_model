b176b23d8646975cf70b0f9834bf222452cca1ee
Carina Silva <carina.silva@estesl.ipl.pt>
2019-10-29 14:47:08 +0000

    Update Find_F.m

--- Find_F.m
+++ Find_F.m
@@ -1,7 +1,5 @@
 function [F] = Find_F(FLEPtarg)  
 
-
-
 % Table lookup to find the desired value of F given a gonochore FLEP
 
 load 'FLEPmat.mat' ;    
@@ -11,6 +9,4 @@ Ftmp = abs(FLEPmat(:,2) - FLEPtarg) ;
 index = find(Ftmp == min(Ftmp),1) ;
 
 F = FLEPmat(index,1) ;
-
-
 
